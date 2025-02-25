f67a97ecc0d42ceaf705befc7fc6b97a82cc6338
pgrochal <pgrochal@cisco.com>
2025-02-25 06:09:38 +0100

    Change union parsing from left-to-right to a best-match approach

--- packages/catalystwan-core/src/catalystwan/core/models/deserialize.py
+++ packages/catalystwan-core/src/catalystwan/core/models/deserialize.py
@@ -1,14 +1,15 @@
 from collections import deque
 from copy import deepcopy
-from dataclasses import fields, is_dataclass
+from dataclasses import dataclass, fields, is_dataclass
 from functools import reduce
 from inspect import isclass, unwrap
-from typing import Any, Dict, List, Literal, Protocol, Tuple, Type, TypeVar, Union
+from typing import Any, Dict, List, Literal, Optional, Protocol, Tuple, Type, TypeVar, Union, cast
 
 from catalystwan.core.exceptions import (
     CatalystwanModelInputException,
     CatalystwanModelValidationError,
 )
+from catalystwan.core.models.utils import count_matching_keys
 from catalystwan.core.types import MODEL_TYPES, AliasPath, DataclassInstance
 from typing_extensions import Annotated, get_args, get_origin, get_type_hints
 
@@ -17,6 +18,13 @@ T = TypeVar("T", bound=DataclassInstance)
 
 class ValueExtractorCallable(Protocol):
     def __call__(self, field_value: Any) -> Any: ...
+
+
+@dataclass
+class ExtractedValue:
+    value: Any
+    exact_match: bool
+    matched_keys: Optional[int] = None
 
 
 class ModelDeserializer:
@@ -62,62 +70,92 @@ class ModelDeserializer:
             return True
         return False
 
-    def __extract_type(self, field_type: Any, field_value: Any, field_name: str) -> Any:
+    def __extract_type(self, field_type: Any, field_value: Any, field_name: str) -> ExtractedValue:
         origin = get_origin(field_type)
         # check for simple types and classes
         if origin is None:
-            if field_type is Any:
-                return field_value
-            if isinstance(field_value, field_type):
-                return field_value
+            if field_type is Any or isinstance(field_value, field_type):
+                return ExtractedValue(value=field_value, exact_match=True)
             elif is_dataclass(field_type):
-                assert isinstance(field_type, type)
-                return deserialize(field_type, **field_value)
+                return ExtractedValue(
+                    value=deserialize(cast(DataclassInstance, field_type), **field_value),
+                    exact_match=False,
+                    matched_keys=count_matching_keys(
+                        cast(DataclassInstance, field_type), field_value
+                    ),
+                )
             elif isclass(unwrap(field_type)):
                 if isinstance(field_value, dict):
-                    return field_type(**field_value)
+                    return ExtractedValue(value=field_type(**field_value), exact_match=False)
                 else:
                     try:
-                        return field_type(field_value)
+                        return ExtractedValue(value=field_type(field_value), exact_match=False)
                     except ValueError:
                         raise CatalystwanModelInputException(
                             f"Unable to match or cast input value for {field_name} [expected_type={unwrap(field_type)}, input={field_value}, input_type={type(field_value)}]"
                         )
+        # List is an exact match only if all of its elements are
         elif origin is list:
             if isinstance(field_value, list):
-                return [
-                    self.__extract_type(get_args(field_type)[0], value, field_name)
-                    for value in field_value
-                ]
+                values = []
+                exact_match = True
+                for value in field_value:
+                    extracted_value = self.__extract_type(
+                        get_args(field_type)[0], value, field_name
+                    )
+                    values.append(extracted_value.value)
+                    if not extracted_value.exact_match:
+                        exact_match = False
+                return ExtractedValue(value=values, exact_match=exact_match)
         elif self.__is_optional(field_type):
             if field_value is None:
-                return None
+                return ExtractedValue(value=None, exact_match=True)
             else:
                 try:
                     return self.__extract_type(get_args(field_type)[0], field_value, field_name)
                 except CatalystwanModelInputException as e:
                     if not field_value:
-                        return None
+                        return ExtractedValue(value=None, exact_match=False)
                     raise e
         elif origin is Literal:
             for arg in get_args(field_type):
                 try:
                     if type(arg)(field_value) == arg:
-                        return type(arg)(field_value)
+                        return ExtractedValue(
+                            value=type(arg)(field_value), exact_match=type(arg) is type(field_value)
+                        )
                 except Exception:
                     continue
         elif origin is Annotated:
             validator, caster = field_type.__metadata__
             if validator(field_value):
-                return field_value
-            return caster(field_value)
-        # TODO: Currently, casting is done left-to-right. Searching deeper for a better match may be the way to go.
+                return ExtractedValue(value=field_value, exact_match=True)
+            return ExtractedValue(value=caster(field_value), exact_match=False)
+        # When parsing Unions, try to find the best match. Currently, it involves:
+        # 1. Finding the exact match
+        # 2. If not found, favors dataclasses - sorted by number of matched keys
+        # 3. If no dataclasses are present, return the leftmost matched argument
         elif origin is Union:
+            matches: List[ExtractedValue] = []
             for arg in get_args(field_type):
                 try:
-                    return self.__extract_type(arg, field_value, field_name)
+                    extracted_value = self.__extract_type(arg, field_value, field_name)
+                    # exact match, return
+                    if extracted_value.exact_match:
+                        return extracted_value
+                    else:
+                        matches.append(extracted_value)
                 except Exception:
                     continue
+            # Only one element matched, return
+            if len(matches) == 1:
+                return matches[0]
+            # Only non-exact matches left, sort and return first element
+            elif len(matches) > 1:
+                matches.sort(
+                    key=lambda x: (x.matched_keys is not None, x.matched_keys), reverse=True
+                )
+                return matches[0]
         # Correct type not found, add exception
         raise CatalystwanModelInputException(
             f"Unable to match or cast input value for {field_name} [expected_type={unwrap(field_type)}, input={field_value}, input_type={type(field_value)}]"
@@ -140,7 +178,9 @@ class ModelDeserializer:
                 field_value = args_copy.popleft()
                 try:
                     new_args.append(
-                        self.__extract_type(field_type, value_extractor(field_value), field.name)
+                        self.__extract_type(
+                            field_type, value_extractor(field_value), field.name
+                        ).value
                     )
                 except (
                     CatalystwanModelInputException,
@@ -164,7 +204,7 @@ class ModelDeserializer:
             try:
                 new_kwargs[field.name] = self.__extract_type(
                     field_type, value_extractor(field_value), field.name
-                )
+                ).value
             except (
                 CatalystwanModelInputException,
                 CatalystwanModelValidationError,
