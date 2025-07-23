d67035f59b418db0ab56130682228833999fe140
Piotr Smialkowski <p.smialkowski@gmail.com>
2025-07-23 11:25:29 +0100

    Change tests env: SDWAN_URL by SDWAN_HOST

--- packages/catalystwan-core/src/catalystwan/core/tests.py
+++ packages/catalystwan-core/src/catalystwan/core/tests.py
@@ -6,11 +6,11 @@ from catalystwan.core.request_adapter import RequestAdapter
 
 @pytest.fixture(scope="package")
 def catalystwan_requests():
-    url = os.environ["SDWAN_URL"]
+    host = os.environ["SDWAN_HOST"]
     port = int(os.environ["SDWAN_PORT"])
     username = os.environ["SDWAN_USERNAME"]
     password = os.environ["SDWAN_PASSWORD"]
-    print(f"Connecting to {url}:{port}...")
-    with create_manager_session(url, username, password, port) as session:
+    print(f"Connecting to {host}:{port}...")
+    with create_manager_session(host, username, password, port) as session:
         session.request_timeout = 60
         yield RequestAdapter(session=session)
