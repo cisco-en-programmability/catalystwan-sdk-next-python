0fa597e774f78341f957d56cecfc64c6de1d3302
Piotr Smialkowski <p.smialkowski@gmail.com>
2025-08-11 14:17:11 +0100

    Add pytest client plugin

--- packages/catalystwan-core/src/catalystwan/core/tests.py
+++ packages/catalystwan-core/src/catalystwan/core/tests.py
@@ -1,8 +1,13 @@
+import importlib
+import logging
 import os
-import pytest
 
-from catalystwan.core.session import create_manager_session
+import pytest
 from catalystwan.core.request_adapter import RequestAdapter
+from catalystwan.core.session import create_manager_session
+
+logger = logging.getLogger(__name__)
+
 
 @pytest.fixture(scope="package")
 def catalystwan_requests():
@@ -10,7 +15,26 @@ def catalystwan_requests():
     port = int(os.environ["SDWAN_PORT"])
     username = os.environ["SDWAN_USERNAME"]
     password = os.environ["SDWAN_PASSWORD"]
-    print(f"Connecting to {host}:{port}...")
+    logger.info(f"Connecting to {host}:{port}...")
     with create_manager_session(host, username, password, port) as session:
         session.request_timeout = 60
         yield RequestAdapter(session=session)
+
+
+@pytest.fixture(scope="package")
+def catalystwan_client_factory(catalystwan_requests):
+    def wrapper(version: str):
+        ver = version.replace(".", "_")
+        name = f"catalystwan.versions.v{ver}.api_client"
+        logger.info(f"Create {name}.ApiClient")
+        module = importlib.import_module(name)
+        ApiClient = getattr(module, "ApiClient")
+        return ApiClient(catalystwan_requests)
+
+    return wrapper
+
+
+@pytest.fixture(scope="package")
+def catalystwan_client(catalystwan_client_factory):
+    version = os.environ["SDWAN_VERSION"]
+    return catalystwan_client_factory(version)
